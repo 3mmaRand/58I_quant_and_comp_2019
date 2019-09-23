5d4a8b8904ad6559e5a68427fae7811e15e8ad42
3mmaRand <emma.rand@york.ac.uk>
2019-09-23 13:20:33 +0100

    encode test

--- lecture1.R
+++ lecture1.R
@@ -1,5 +1,24 @@
 library(tidyverse)
 library(multcomp)
+library(Rmisc)
+
+stag <- read.table("../data/stag.txt", header = TRUE)
+mod <- lm(data = stag, mand ~ jh)
+
+ggplot(data = stag, aes(x = jh, y = mand)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  ylim(0, 1.8) +
+  theme_classic()
+
+chaff <- read.table("../data/chaff.txt", header = T)
+chaff <- chaff %>% gather(key = sex, value = mass)
+chaffsum <- summarySE(data = chaff, measurevar = "mass", groupvars = "sex")
+ggplot(data = chaffsum, aes(x = sex, y = mass)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = mass - se, ymax = mass + se), width = .1) +
+  theme_classic()
+
 
 # lm() example, one-way anova equivalent, colony diameter
 # grown on three different media
